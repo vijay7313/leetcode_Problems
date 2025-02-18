95531ee114049082c78f2932ba9bebb3b37075ae
vijay7313 <vraja7313@gmail.com>
2025-02-18 10:52:39 +0530

    Binary tree and Binary search tree separation.

--- src/treeMedium/BTree.java
+++ src/treeMedium/BTree.java
@@ -1,7 +1,11 @@
 package treeMedium;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
+
+import treeMedium.BSTree.TreeNode;
 
 public class BTree {
 
@@ -68,4 +72,108 @@ public class BTree {
 		return size;
 	}
 
+	public List<Integer> inOrderTraversal() {
+
+		List<Integer> list = new ArrayList<>(size);
+
+		inOrderTraversal(root, list);
+
+		return list;
+	}
+
+	private void inOrderTraversal(TreeNode root, List<Integer> list) {
+
+		if (isEmpty(root))
+			return;
+
+		inOrderTraversal(root.left, list);
+
+		list.add(root.val);
+
+		inOrderTraversal(root.right, list);
+	}
+
+	public int heightOfTree() {
+
+		return heightOfTree(root);
+
+	}
+
+	private int heightOfTree(TreeNode root) {
+
+		if (root == null)
+			return 0;
+
+		if (isLeaf(root))
+			return 1;
+
+		int left = heightOfTree(root.left);
+		int right = heightOfTree(root.right);
+
+		return 1 + Math.max(left, right);
+	}
+
+	private boolean isLeaf(TreeNode root) {
+		return root.left == null && root.right == null;
+	}
+
+	public List<Integer> levelOrderTraversal() {
+
+		List<Integer> result = new ArrayList<>();
+
+		if (isEmpty())
+			return result;
+
+		Queue<TreeNode> queue = new LinkedList<>();
+
+		queue.add(root);
+
+		while (!queue.isEmpty()) {
+
+			TreeNode current = queue.poll();
+			result.add(current.val);
+
+			if (current.left != null)
+				queue.add(current.left);
+
+			if (current.right != null)
+				queue.add(current.right);
+		}
+		return result;
+	}
+
+	public List<List<Integer>> binaryTreeZigzagLevelOrderTraversal() {
+
+		List<List<Integer>> list = new ArrayList<>();
+
+		for (int level = 0; level < heightOfTree(); level++) {
+
+			boolean zigZag = level % 2 == 0 ? false : true;
+
+			list.add(binaryTreeZigzagLevelOrderTraversal(root, new ArrayList<>(), level, zigZag));
+
+		}
+
+		return list;
+	}
+
+	private List<Integer> binaryTreeZigzagLevelOrderTraversal(TreeNode root, List<Integer> list, int level,
+			boolean zigZag) {
+
+		if (root == null)
+			return null;
+
+		if (level == 0)
+			list.add(root.val);
+
+		if (zigZag) {
+			binaryTreeZigzagLevelOrderTraversal(root.right, list, level - 1, zigZag);
+			binaryTreeZigzagLevelOrderTraversal(root.left, list, level - 1, zigZag);
+		} else {
+			binaryTreeZigzagLevelOrderTraversal(root.left, list, level - 1, zigZag);
+			binaryTreeZigzagLevelOrderTraversal(root.right, list, level - 1, zigZag);
+		}
+
+		return list;
+	}
 }
