97f0be97e341ac059635a794b9c81092c93fa748
vijay7313 <vraja7313@gmail.com>
2025-03-02 20:55:45 +0530

    Improved 'Double a Number Represented as a Linked List' with better time using recursion.

--- src/linkedListMedium/DoubleANumberRepresentedAsList.java
+++ src/linkedListMedium/DoubleANumberRepresentedAsList.java
@@ -6,15 +6,21 @@ import linkedListMedium.LinkedList.ListNode;
 
 public class DoubleANumberRepresentedAsList {
 	public static void main(String[] args) {
-		int[] nums = { 2, 9, 6 };
+		int[] nums = { 1, 8, 9 };
 
 		LinkedList list = new LinkedList();
 
 		for (int num : nums)
 			list.add(num);
 
-		ListNode node = list.DoubleANumberInList();
-
+//		ListNode node = list.DoubleANumberInList();
+//
+//		int[] array = list.toArray(node);
+//
+//		System.out.println(Arrays.toString(array));
+		
+		ListNode node = list.doubleANumberInListUsingRecursion();
+		
 		int[] array = list.toArray(node);
 
 		System.out.println(Arrays.toString(array));
